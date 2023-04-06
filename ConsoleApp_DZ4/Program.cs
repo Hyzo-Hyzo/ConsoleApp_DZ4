e0fa3a6a10f26b0cb23680b33e789f40f659e2c7
Ira <mazuririnochkacat@gmail.com>
2023-04-06 16:13:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp_DZ4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_DZ4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
